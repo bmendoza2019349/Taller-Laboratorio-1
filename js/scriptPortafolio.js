17e2224c2d79c92448d2c022393552826e074d9a
bmendoza-2019349 <bmendoza-2019349@kinal.edu.gt>
2024-01-24 18:41:06 -0600

    Se modifico portafolio

---
+++ js/scriptPortafolio.js
@@ -0,0 +1,12 @@
+function enviarCorreo() {
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var mensaje = document.getElementById("mensaje").value;
+    
+    var link = "mailto:bmendoza-2019349@kinal.edu.gt"
+             + "?subject=" + encodeURIComponent("Nuevo mensaje de contacto")
+             + "&body=" + encodeURIComponent("Nombre: " + nombre + "\n\nEmail: " + email + "\n\nMensaje: " + mensaje);
+    
+    window.location.href = link;
+    alert("Se envio el correo");
+}
