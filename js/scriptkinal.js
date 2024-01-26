30e6f4cac87a8e4471bca77f4a95315a32d84bc5
bmendoza-2019349 <bmendoza-2019349@kinal.edu.gt>
2024-01-25 20:12:00 -0600

    Se agregaron imagenes y se modificaron los archivos kinal

---
+++ js/scriptkinal.js
@@ -0,0 +1,100 @@
+function ventanaBasicos() {
+    var ventana = document.getElementById("ventanaBasicos");
+    ventana.style.display = "block";
+}
+
+function cerrarventanaBasicos() {
+    var ventana = document.getElementById("ventanaBasicos");
+    ventana.style.display = "none";
+}
+
+function ventanaDiver() {
+    var ventana = document.getElementById("ventanaDiver");
+    ventana.style.display = "block";
+}
+
+function cerrarventanaDiver() {
+    var ventana = document.getElementById("ventanaDiver");
+    ventana.style.display = "none";
+}
+
+function ventanaTecnica() {
+    var ventana = document.getElementById("ventanaTecnica");
+    ventana.style.display = "block";
+}
+
+function cerrarventanaTecnica() {
+    var ventana = document.getElementById("ventanaTecnica");
+    ventana.style.display = "none";
+}
+
+function ventanaTecno() {
+    var ventana = document.getElementById("ventanaTecno");
+    ventana.style.display = "block";
+}
+
+function cerrarventanaTecno() {
+    var ventana = document.getElementById("ventanaTecno");
+    ventana.style.display = "none";
+}
+
+/*Ventanas Emergentes externas*/ 
+function ventanaTrabajos() {
+    var ventana = document.getElementById("ventanaTrabajos");
+    ventana.style.display = "block";
+}
+
+function cerrarventanaTrabajos() {
+    var ventana = document.getElementById("ventanaTrabajos");
+    ventana.style.display = "none";
+}
+
+function ventanaSponsors() {
+    var ventana = document.getElementById("ventanaSponsors");
+    ventana.style.display = "block";
+}
+
+function cerrarventanaSponsors() {
+    var ventana = document.getElementById("ventanaSponsors");
+    ventana.style.display = "none";
+}
+
+function ventanaFundacion() {
+    var ventana = document.getElementById("ventanaFundacion");
+    ventana.style.display = "block";
+}
+
+function cerrarventanaFundacion() {
+    var ventana = document.getElementById("ventanaFundacion");
+    ventana.style.display = "none";
+}
+function ventanaPatronato() {
+    var ventana = document.getElementById("ventanaPatronato");
+    ventana.style.display = "block";
+}
+
+function cerrarventanaPatronato() {
+    var ventana = document.getElementById("ventanaPatronato");
+    ventana.style.display = "none";
+}
+function ventanaPress() {
+    var ventana = document.getElementById("ventanaPress");
+    ventana.style.display = "block";
+}
+
+function cerrarventanaPress() {
+    var ventana = document.getElementById("ventanaPress");
+    ventana.style.display = "none";
+}
+function ventanaBlog() {
+    var ventana = document.getElementById("ventanaBlog");
+    ventana.style.display = "block";
+}
+
+function cerrarventanaBlog() {
+    var ventana = document.getElementById("ventanaBlog");
+    ventana.style.display = "none";
+}
+function irAPagina(pagina) {
+    window.location.href = pagina;
+}
